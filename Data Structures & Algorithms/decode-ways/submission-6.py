6257fece43e9fdf6b876d38450ae69fa7100a302
fasil729 <101599549+fasil729@users.noreply.github.com>
2026-08-11 23:58:07 +0300

    Add: decode-ways - submission-6

---
+++ Data Structures & Algorithms/decode-ways/submission-6.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        seen = [None] * n
+
+        def dp(ind):
+            if ind < n and s[ind] == '0':
+                return 0
+            if ind >= n - 1:
+                return 1
+            if seen[ind] != None:
+                return seen[ind]
+
+            
+
+            seen[ind] = dp(ind + 1)
+            seen[ind] += dp(ind + 2) if int(s[ind: ind + 2]) <= 26 else 0
+            return seen[ind]
+        
+        return dp(0)
+            
+
+
+        
